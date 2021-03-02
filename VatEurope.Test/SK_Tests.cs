d86d71fa08fe9db6377a1c2b09fc5ab64bad97d3
MS <simon@mauracher.cc>
2021-03-02 22:06:47 +0100

    added Slovakia

---
+++ VatEurope.Test/SK_Tests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace VatEurope.Test
+{
+    [TestFixture]
+    public sealed class SK_Tests
+    {
+        private readonly CountryEnum _country = CountryEnum.Slovakia;
+
+        [TestCase("SK4030000007")]
+        public void TestValidVat(string validVat)
+        {
+            _country.IsValidChecksum(validVat).Should().BeTrue();
+        }
+
+        [TestCase("nonono")]
+        [TestCase("SK403000000")]
+        [TestCase("SK40300000077")]
+        [TestCase("SK4030000003")]
+        [TestCase("SK5407062531")]
+        public void TestInvalidVat(string invalidVat)
+        {
+            _country.IsValidChecksum(invalidVat).Should().BeFalse();
+        }
+    }
+}
---
+++ VatEurope/Checksum/SK_Checksum.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VatEurope.Checksum
+{
+    internal sealed class SK_Checksum : IChecksum
+    {
+        private const string _regexFullString = @"^SK[1-9][0-9][234789][0-9]{7}$";
+
+        public bool IsValidChecksum(string vat)
+        {
+            if (string.IsNullOrEmpty(vat))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(vat, _regexFullString))
+            {
+                return false;
+            }
+
+            return long.Parse(vat.Substring(2, 10)) % 11 == 0;
+        }
+    }
+}
